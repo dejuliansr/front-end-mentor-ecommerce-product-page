8d3454287ac81dd858c6530eb36b252813ff033b
dejuliansr <demetriojulian99@gmail.com>
2024-10-11 17:03:17 +0700

    fix: missing image

--- js/index.js
+++ js/index.js
@@ -82,10 +82,10 @@ const lightboxMainImg = document.getElementById('lightbox-img');
 const lightboxMainImgMobile = document.getElementById('lightbox-img-mobile');
 const lightboxThumbs = document.querySelectorAll('.lightbox-thumb');
 const productImages = [
-  '../images/image-product-1.jpg',
-  '../images/image-product-2.jpg',
-  '../images/image-product-3.jpg',
-  '../images/image-product-4.jpg'
+  'images/image-product-1.jpg',
+  'images/image-product-2.jpg',
+  'images/image-product-3.jpg',
+  'images/image-product-4.jpg'
 ];
 let currentImageIndex = 0;
 const prevBtn = document.getElementById('prev-btn');
@@ -221,14 +221,14 @@ function updateCartDetails() {
       const li = document.createElement('li');
       li.innerHTML = `
         <div class="cart-item">
-          <img src="../images/image-product-1.jpg" alt="${item.name}">
+          <img src="images/image-product-1.jpg" alt="${item.name}">
           <div class="item-details">
             <span>${item.name}</span>
             <span>$${item.price} x ${item.quantity} <span style="font-weight: 700;">$${(item.price * item.quantity).toFixed(2)}</span></span>
 
           </div>
           <button class="remove-item" onclick="removeItem('${item.name}')">
-            <img src="../images/icon-delete.svg" alt="delete">
+            <img src="images/icon-delete.svg" alt="delete">
           </button>
         </div>
       `;
